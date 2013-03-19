c5104849428ac21ddaba21cfe6c0858699b11c0c
Bruce Lv <lvkun2006@gmail.com>
2013-03-19 23:38:31 +0800

    trigram without stop

--- h1-p/tagger.py
+++ h1-p/tagger.py
@@ -48,14 +48,6 @@ def unigram_tag(common_file, test_file, counts_file):
     for word in common_file:
         common_words.add(word.strip())
 
-    total_O_count = 0
-    total_GENE_count = 0
-    for k,v in hmm.emission_counts.iteritems():
-        if k[1] == "O":
-            total_O_count += v
-        else:
-            total_GENE_count += v
-
     for line in test_file:
         word = line.strip()
 
@@ -71,28 +63,67 @@ def unigram_tag(common_file, test_file, counts_file):
             GENE_count = hmm.emission_counts[(word, "I-GENE")]
 
         tag = ""
-        if O_count/total_O_count > GENE_count/total_GENE_count:
+        if O_count/hmm.ngram_counts[0][("O",)] > GENE_count/hmm.ngram_counts[0][("I-GENE",)] :
             tag = 'O'
         else:
             tag = 'I-GENE'
 
         print "%s %s" % (word, tag)
 
+import math
+
 class Tag_Node:
 
     def __init__(self, word, tag):
         self.word = word
         self.tag = tag
-        self.hmm = hmm
+        self.value = 0
+        self.prev = None
 
-    def search(self, prev_tags, hmm):
-        pass
+    def search(self, prev_nodes, hmm):
+        if len(prev_nodes) == 0:
+            self.value = math.log(self.get_trigram_prob(hmm, "*", "*", self.tag)) + math.log(self.get_emission_prob(hmm))
+            return
+
+        max_prev_node = None
+        max_value = 0
+        for node in prev_nodes:
+            curr_value = 0
+            if node.prev:
+                curr_value = node.value + math.log(self.get_trigram_prob(hmm, node.prev.tag, node.tag, self.tag)) + math.log(self.get_emission_prob(hmm))
+            else:
+                curr_value = node.value + math.log(self.get_trigram_prob(hmm, "*", node.tag, self.tag)) + math.log(self.get_emission_prob(hmm))
+
+            if curr_value > max_value:
+                max_prev_node = node
+                max_value = value
+
+        self.prev = max_prev_node
+        self.value = max_value
+
+    def get_emission_prob(self, hmm):
+        count_word_tag = hmm.emission_counts[(self.word, self.tag)]
+
+        return count_word_tag / hmm.ngram_counts[0][(self.tag,)]
+
+    def get_trigram_prob(self, hmm, yi_2, yi_1, yi):
+        trigram_count = hmm.ngram_counts[2][(yi_2, yi_1, yi)]
+        bigram_count = hmm.ngram_counts[1][(yi_2, yi_1)]
+        return trigram_count / bigram_count
 
 def print_tag_result(node_list):
-    pass
+    print node_list
 
-def create_nodes_from_word(word):
-    return [Tag_Node(word, "O"), Tag_Node(word, "I-GENE")]
+def create_nodes_from_word(hmm, common_words, word):
+    if word not in common_words:
+        word = "_RARE_"
+    
+    ret = []
+    for tag in hmm.all_states:
+        if hmm.emission_counts[word, tag] > 0:
+            ret.append(Tag_Node(word, tag))
+
+    return ret
 
 def trigram_tag(common_file, test_file, counts_file):
     hmm = Hmm(3)
@@ -102,38 +133,32 @@ def trigram_tag(common_file, test_file, counts_file):
     for word in common_file:
         common_words.add(word.strip())
 
-    total_O_count = 0
-    total_GENE_count = 0
-    for k,v in hmm.emission_counts.iteritems():
-        if k[1] == "O":
-            total_O_count += v
-        else:
-            total_GENE_count += v
-
     node_list = []
     index = 0
     for line in test_file:
         word = line.strip()
 
         if len(word) == 0:
-
             print_tag_result(node_list)
             node_list = []
-            index = 0
             continue
 
-        nodes = create_nodes_from_word(word)
+        nodes = create_nodes_from_word(hmm, common_words, word)
         prev_nodes = []
-        if index > 0:
+        if len(node_list) > 0:
             prev_nodes = node_list[index - 1]
 
         for node in nodes:
-            node.search(prev_tags)
+            node.search(prev_nodes, hmm)
+
+        node_list.append(nodes)
+
+    if len(node_list) > 0:
+        print_tag_result(node_list)
 
 import argparse
 
 if __name__ == '__main__':
-
     parser = argparse.ArgumentParser(description='a simple tagger implement')
     parser.add_argument('action', metavar='action',
                         help="""the action should be performed, possible action:
@@ -167,3 +192,8 @@ if __name__ == '__main__':
         else:
             parser.print_usage()
 
+    elif args.action == "trigram_tag":
+        if args.common_file and args.test_file and args.counts_file:
+            trigram_tag(args.common_file, args.test_file, args.counts_file)
+        else:
+            parser.print_usage()
